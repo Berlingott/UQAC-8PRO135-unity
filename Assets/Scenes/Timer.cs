3b3ef33dbaf7badf281eb34371308a5f0a906d81
Berlingott <simsimondu@gmail.com>
2021-10-06 11:06:00 -0400

    Add level1 scene and countdown timer

---
+++ Assets/Scenes/Timer.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    public int countdownTime;
+    public  Text countdownDisplay;
+
+    private void Start()
+    {
+        StartCoroutine(CountdownToStart());
+    }
+
+    IEnumerator CountdownToStart()
+    {
+        while (countdownTime > 0)
+        {
+            countdownDisplay.text = countdownTime.ToString();
+
+            yield return new WaitForSeconds(1f);
+
+            countdownTime--;
+        }
+
+        countdownDisplay.text = "0";
+
+    }
+}
